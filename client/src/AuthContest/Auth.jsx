bd2b1f2825204195e11023be982876a3b8c09fc3
guna .k <gunakamaraj0@gmail.com>
2025-05-29 15:18:00 +0530

    changed

---
+++ client/src/AuthContest/Auth.jsx
@@ -0,0 +1,66 @@
+import React, { Children, createContext, useState } from 'react'
+import userService from '../service/service';
+import { Navigate, useNavigate } from 'react-router';
+import Swal from 'sweetalert2'
+
+const Auth=createContext();
+// const Swal=require('sweetalert2');
+function AuthProvider({children}){
+    const [isAuth,setIsauth]=useState(false);
+    const [email,setEmail]=useState('');
+    const [password,setPassword]=useState('');
+    const navigate=useNavigate();
+    function login(email,password){
+        try{
+            userService.authenticate(email,password)
+            setIsauth(true)
+            Swal.fire({
+            title: "Good job!",
+            text: "You are LoggedIn",
+            icon: "success"
+            });
+            navigate('/Home')
+        }catch(error){
+            Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: `${error}`,
+        
+});
+        }
+    }
+    function logout(){
+        setIsauth(false);
+        navigate('/Home')
+    }
+    function addUser(email,password){
+        try{
+            userService.addUser(email,password);
+            setIsauth(true)
+            Swal.fire({
+            title: "Good job!",
+            text: "You are LoggedIn",
+            icon: "success"
+            });
+            navigate('/Home')
+        }catch(error){
+                 Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: `${error}`,
+        
+});
+        }
+        
+        
+    }
+    return(
+        <>
+        <Auth.Provider value={{email, password, setEmail, setPassword, login, logout, addUser }}>
+            {children}
+        </Auth.Provider>
+        </>
+    )
+}
+export {AuthProvider};
+export default Auth;
